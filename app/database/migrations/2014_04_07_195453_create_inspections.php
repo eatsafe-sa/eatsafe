60eb04c3064a8846588260a8a69def64a0ec43c0
travtex <tflatt74@gmail.com>
2014-04-07 15:28:48 -0500

    added db migrations to create tables and association

---
+++ app/database/migrations/2014_04_07_195453_create_inspections.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInspections extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('inspections', function($table)
+		{
+			$table->increments('id');
+			$table->string('last_date', 25);
+			$table->integer('demerits');
+			$table->string('url', 100);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::dropIfExists('inspections');
+	}
+
+}
