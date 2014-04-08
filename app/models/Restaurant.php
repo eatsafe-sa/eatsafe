132b61ef26e770d9486cbdbf1afacc89ec072961
travtex <tflatt74@gmail.com>
2014-04-08 12:03:18 -0500

    sanitizing json data

--- app/models/Restaurant.php
+++ app/models/Restaurant.php
@@ -16,4 +16,24 @@ class Restaurant extends Eloquent
 	    'address' => 'required|max:100',
 	    'zip' => 'required|min:5'
 	);
+
+	public function setNameAttribute($value)
+	{
+		$this->attributes['name'] = strtolower($value);
+	}
+
+	public function getNameAttribute()
+	{
+		return ucfirst($this->attributes['name']);
+	}
+
+	public function setAddressAttribute($value)
+	{
+		$this->attributes['address'] = strtolower($value);
+	}
+
+	public function getAddressAttribute()
+	{
+		return ucfirst($this->attributes['address']);
+	}
 }
