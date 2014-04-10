9ddf2431162d44e4e54f99e60bd6a60292ad89c3
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-10 10:07:59 -0500

    deleted reg html open form

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 {{ Form::open(array('action' => 'HomeController@doLogin', 'class' => 'form-signin')) }}
-<form class="form-signin" role="form">
     <h2 class="form-signin-heading">EatSafe-SA Administrator Login</h2>
    	{{ Form::text('email', $value = null, $attributes = array ('placeholder' => 'E-Mail Address','class' => 'requiredField form-input','autofocus' => 'autofocus'))}}
     {{ Form::password('password', array ('placeholder' => 'Password','class' => 'requiredField form-input'))}}
