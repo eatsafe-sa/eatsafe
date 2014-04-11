394ba40beffc25387913c97ade45850db1367160
travtex <tflatt74@gmail.com>
2014-04-11 15:04:36 -0500

    social icon css / temp team view

--- app/views/team.blade.php
+++ app/views/team.blade.php
@@ -6,26 +6,29 @@
 
 @section('content')
 
-<div>
+<div class="container">
 	<!-- Three columns of text below the carousel -->
       <div class="row">
-        <div class="col-lg-4">
-          <img class="img-circle" src="/img/travis.jpg" alt="Generic placeholder image">
+        <div class="col-sm-4">
+          <img class="img-circle profile-pic" src="/img/travis.jpg" alt="Generic placeholder image">
           <h2>Travis</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-globe"></span>GitHub</button>
+          <a class="btn btn-social-icon btn-lg btn-github" href="https://github.com/travtex" target="_blank"><i class="fa fa-github-alt"></i></a>
+          <a class="btn btn-social-icon btn-lg btn-linkedin" href="https://www.linkedin.com/in/travisflatt" target="_blank"><i class="fa fa-linkedin"></i></a>
         </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="img-circle" src="/img/mario.jpg" alt="Generic placeholder image">
+        <div class="col-sm-4">
+          <img class="img-circle profile-pic" src="/img/mario.jpg" alt="Generic placeholder image">
           <h2>Mario</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-globe"></span>GitHub</button>
+          <a class="btn btn-social-icon btn-lg btn-github" href="https://github.com/mariom1231" target="_blank" ><i class="fa fa-github-alt"></i></a>
+          <a class="btn btn-social-icon btn-lg btn-linkedin" href="http://www.linkedin.com/in/mariom1231/" target="_blank"><i class="fa fa-linkedin"></i></a>
         </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="img-circle" src="/img/chris.jpg" alt="Generic placeholder image">
+        <div class="col-sm-4">
+          <img class="img-circle profile-pic" src="/img/chris.jpg" alt="Generic placeholder image">
           <h2>Chris</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-globe"></span>GitHub</button>
+          <a class="btn btn-social-icon btn-lg btn-github" href="https://github.com/Chris-Reyna" target="_blank" ><i class="fa fa-github-alt"></i></a>
+          <a class="btn btn-social-icon btn-lg btn-linkedin" href="https://www.linkedin.com/in/christopherreyna" target="_blank"><i class="fa fa-linkedin"></i></a>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
 </div>
