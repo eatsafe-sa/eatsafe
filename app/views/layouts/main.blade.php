69835dc95cc67853352d58e4f195de3f8d7a8edc
mariom1231 <mamoren1@gmail.com>
2014-04-15 14:20:47 -0500

    moved flash msg to main blade

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -40,6 +40,12 @@
  </head>
  <body>
     @yield('content')
+    @if (Session::has('successMessage'))
+            <div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
+          @endif
+          @if (Session::has('errorMessage'))
+            <div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
+          @endif
 
     @yield('bottomscript')
 </body>
