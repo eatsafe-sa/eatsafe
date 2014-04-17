2f81a08c3f71f2525df2b6540dead6dc0d181491
travtex <tflatt74@gmail.com>
2014-04-17 13:07:39 -0500

    user edits added

--- app/views/users/edit.blade.php
+++ app/views/users/edit.blade.php
@@ -15,24 +15,28 @@
 		<div class="well col-sm-8">
 			<h2 class="text-center">Edit User Information</h2>
 			<p>All fields are required</p>
-			{{ Form::model($user, array('action' => array('UsersController@update', $user->id), 'method' => 'PUT')) }}
+			{{ Form::model($user, array('action' => array('UsersController@update', $user->id), 'method' => 'PUT', 'role' => 'form', 'class' => 'form')) }}
 			{{ $errors->first('email', '<h3>Must Have an E-mail.</h3>')}}
-						<div class="col-sm-12">
+						<div class="col-sm-12 form-group">
 							
 							{{ Form::text('email', $value = null, $attributes = array (
 								'placeholder' => 'E-mail *',
-								'class' => 'requiredField form-control'
+								'class' => 'form-control'
 							))}}
 						</div>
 						
-						<div class="col-sm-12">
+						<div class="col-sm-12 form-group">
 					    {{ $errors->first('password', '<h3>Must Have a Password.</h3>')}}
 					    {{ Form::password('password', array (
 							'placeholder' => 'Password *',
-							'class' => 'requiredField form-control'
+							'class' => 'form-control'
 					    ))}}
 						</div>
-						{{ Form::checkbox('is_admin', true)}}
+						<div class="form-group">
+						<label>
+						{{ Form::checkbox('is_admin', true)}} Add Admin Permissions
+						</label>
+						</div>
 					<input type="submit" class="btn btn-lg btn-success" name="submit" value="Edit User">
 				{{Form::close()}}
 				
